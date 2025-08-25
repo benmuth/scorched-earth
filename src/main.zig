58e7eb16e267e78dda6cbd856025a3486a0d3b95
Stone Chen <chen.stonechen@gmail.com>
2025-08-25 17:06:32 -0400

    terrain now stores an origin
    Store an origin that represents the world position. Then all accesses to terrain should be via world space values.

--- src/main.zig
+++ src/main.zig
@@ -54,6 +54,48 @@ const Tank = struct {
         self.power = std.math.clamp(power, 0.0, 100.0);
     }
 
+    fn setPosition(self: *Tank, terrain: *Terrain, _x: f32, _y: f32) void {
+        var temp_body = self.body;
+        temp_body.x = _x;
+        temp_body.y = _y;
+        temp_body.width -= 1;
+        temp_body.height -= 1;
+
+        for (0..terrain.height) |y| {
+            for (0..terrain.width) |x| {
+                if (terrain.getTerrain(x, y) > 0) {
+                    const terrain_body = rl.Rectangle{
+                        .x = @floatFromInt(x),
+                        .y = @floatFromInt(y),
+                        .width = 1,
+                        .height = 1,
+                    };
+
+                    if (rl.checkCollisionRecs(terrain_body, temp_body)) {
+                        std.log.debug("Collision detected at ({}, {})\n", .{ x, y });
+                        return;
+                    }
+                }
+            }
+        }
+        self.body.x = _x;
+        self.body.y = _y;
+    }
+
+    fn checkSetFalling(self: *Tank, world: *World) void {
+        const y_start: f32 = self.body.y + self.body.height;
+        const x_start: f32 = self.body.x;
+        const x_end: f32 = self.body.x + self.body.width;
+
+        var x = x_start;
+        while (x < x_end) : (x += 1) {
+            if (world.terrain.getTerrainWorld(x, y_start) > 0) {
+                self.falling = false;
+                return;
+            }
+        }
+    }
+
     pub fn update(self: *Tank, world: *World) void {
         const delta_time = rl.getFrameTime();
         for (0..self.num_weapons) |i| {
@@ -68,21 +110,10 @@ const Tank = struct {
                 self.num_weapons -= 1;
             }
         }
-        std.log.debug("num_weapons: {d}", .{self.num_weapons});
-
-        const y_start: u32 = @intFromFloat(self.body.y + self.body.height + 1.0);
-        const x_start: u32 = @intFromFloat(self.body.x);
-        const x_end: u32 = @intFromFloat(self.body.x + self.body.width);
-        for (x_start..x_end) |x| {
-            if (world.terrain.getTerrain(x, y_start) > 0) {
-                self.falling = false;
-            } else {
-                self.falling = true;
-            }
-        }
+        self.checkSetFalling(world);
 
         if (self.falling) {
-            self.body.y += 10;
+            self.setPosition(&(world.*.terrain), self.body.x, self.body.y + 1);
             return;
         }
 
@@ -95,9 +126,9 @@ const Tank = struct {
         }
 
         if (rl.isKeyDown(.right)) {
-            self.body.x += global_speed * delta_time;
+            self.setPosition(&(world.*.terrain), self.body.x + global_speed * delta_time, self.body.y);
         } else if (rl.isKeyDown(.left)) {
-            self.body.x -= global_speed * delta_time;
+            self.setPosition(&(world.*.terrain), self.body.x - global_speed * delta_time, self.body.y);
         }
 
         const aim_speed = 3.0 * delta_time;
@@ -203,14 +234,10 @@ const Weapon = struct {
 
     fn explode(self: *Weapon, world: *World) void {
         const radius: u32 = 50;
-        const x_start: usize = @intFromFloat(self.body.x - radius);
-        const y_start: usize = @intFromFloat(self.body.y - radius);
+        const x_start: usize = if (self.body.x - radius < 0) 0 else @intFromFloat(self.body.x - radius);
+        const y_start: usize = if (self.body.y - radius < 0) 0 else @intFromFloat(self.body.y - radius);
 
-        for (y_start..y_start + 2 * radius) |y| {
-            for (x_start..x_start + 2 * radius) |x| {
-                world.terrain.setTerrain(x, y, 0);
-            }
-        }
+        world.terrain.setTerrainRect(x_start, y_start, radius * 2, radius * 2, 0);
 
         for (0..world.tanks.len) |i| {
             if (i != world.active_tank) {
@@ -238,6 +265,12 @@ const World = struct {
     },
 
     active_tank: usize = 0,
+    camera: rl.Camera2D = .{
+        .offset = .{ .x = world_width / 2, .y = world_height / 2 },
+        .target = .{ .x = 0, .y = world_height / 2 },
+        .rotation = 0.0,
+        .zoom = 3.0,
+    },
 
     pub fn init(self: *World) void {
         self.terrain.init(world_width, world_height, std.heap.page_allocator) catch unreachable;
@@ -258,6 +291,7 @@ const World = struct {
         self.terrain.pre_render();
 
         rl.beginDrawing();
+        rl.beginMode2D(self.camera);
         // rl.clearBackground(.white);
         self.terrain.render();
         for (&self.tanks) |*tank| {
@@ -269,12 +303,14 @@ const World = struct {
             .{ self.tanks[self.active_tank].aim_angle, self.tanks[self.active_tank].power },
         );
         rl.drawText(text, 0, 0, 15, .black);
+        rl.endMode2D();
+
         rl.endDrawing();
     }
 };
 
 pub fn main() anyerror!void {
-    rl.initWindow(world_width, world_height, "Scorched Earth");
+    rl.initWindow(1920, 1080, "Scorched Earth");
     defer rl.closeWindow();
     rl.setTargetFPS(120);
 
--- src/terrain.zig
+++ src/terrain.zig
@@ -7,11 +7,22 @@ const TerrainType = enum(u32) {
 };
 
 pub const Terrain = struct {
+    world_origin_x: f32 = 0,
+    world_origin_y: f32 = 0,
+
     width: u32 = 0,
     height: u32 = 0,
     data: []u32,
     texture_data: []u32,
     texture: rl.RenderTexture2D = undefined,
+
+    pub fn setTerrainRect(self: *Terrain, _x: usize, _y: usize, w: usize, h: usize, t_type: u32) void {
+        for (_y.._y + h) |y| {
+            for (_x.._x + w) |x| {
+                self.setTerrain(x, y, t_type);
+            }
+        }
+    }
 
     pub fn init(self: *Terrain, width: u32, height: u32, allocator: std.mem.Allocator) !void {
         self.width = width;
@@ -23,18 +34,65 @@ pub const Terrain = struct {
         // self.texture.texture.format = rl.PixelFormat.uncompressed_r8g8b8a8;
         self.fillHalf(1);
 
+        self.world_origin_x = 0;
+        self.world_origin_y = 0;
+
         std.log.debug("Terrain initialized: {d}x{d}\n", .{ width, height });
     }
 
+    pub fn worldToIndex(self: *Terrain, world_x: f32, world_y: f32) ?usize {
+        if (world_x < self.world_origin_x or world_y < self.world_origin_y) {
+            return null;
+        }
+        if (world_x >= self.world_origin_x + @as(f32, @floatFromInt(self.width)) or world_y >= self.world_origin_y + @as(f32, @floatFromInt(self.height))) {
+            return null;
+        }
+        const local_x: usize = @intFromFloat(world_x - self.world_origin_x);
+        const local_y: usize = @intFromFloat(world_y - self.world_origin_y);
+
+        return local_y * self.width + local_x;
+    }
+
     pub fn fillHalf(self: *Terrain, t_type: u32) void {
-        const half_height = self.height / 2;
         @memset(self.data[0 .. self.width * self.height], 0);
+
+        const half_height = self.height / 2;
 
         for (half_height..self.height) |y| {
             for (0..self.width) |x| {
                 self.setTerrain(x, y, t_type);
             }
         }
+    }
+
+    pub fn setTerrainWorld(self: *Terrain, world_x: f32, world_y: f32, t_type: u32) void {
+        const index = self.worldToIndex(world_x, world_y);
+        if (index) |i| {
+            self.setTerrainIndex(i, t_type);
+        }
+    }
+
+    pub fn getTerrainWorld(self: *Terrain, world_x: f32, world_y: f32) u32 {
+        const index = self.worldToIndex(world_x, world_y);
+        if (index) |i| {
+            return self.getTerrainIndex(i);
+        } else {
+            return 0;
+        }
+    }
+
+    pub fn setTerrainIndex(self: *Terrain, index: usize, t_type: u32) void {
+        if (index >= self.width * self.height) {
+            return;
+        }
+        self.data[index] = t_type;
+    }
+
+    pub fn getTerrainIndex(self: *Terrain, index: usize) u32 {
+        if (index >= self.width * self.height) {
+            return 0;
+        }
+        return self.data[index];
     }
 
     pub fn setTerrain(self: *Terrain, x: usize, y: usize, t_type: u32) void {
